609866b4b2f382c3d6e7fb5029413913e706e8be
Michael Gutierrez <michaelgutierrezj@gmail.com>
2022-04-15 11:53:10 -0500

    get actions endpoint implemented

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,18 @@
 // Write your "actions" router here!
+const express = require('express')
+
+const Actions = require('./actions-model')
+
+const {} = require('./actions-middlware')
+
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    Actions.get()
+        .then(projects => {
+            res.status(200).json(projects)
+        })
+        .catch(error => next({ error }))
+})
+
+module.exports = router
