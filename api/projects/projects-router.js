4cb862fb6663ead84a7248b9f6a2fb803f99bdad
Michael Gutierrez <michaelgutierrezj@gmail.com>
2022-04-15 10:17:11 -0500

    get projects endpoint implemented

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,17 @@
 // Write your "projects" router here!
+const express = require('express')
+
+const Projects  = require('./projects-model')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    Projects.get()
+        .then(projects => {
+            res.status(200).json(projects)
+        })
+        .catch(err => console.log(err))
+})
+
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,9 +1,18 @@
-const express = require('express');
-const server = express();
-
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+const express = require('express');
+
+const projectsRouter = require('./projects/projects-router')
+
+const server = express();
+
+server.use(express.json())
+server.use('/api/projects', projectsRouter)
+
+server.get('/', (req, res) => {
+    res.send('My Web Api')
+})
 
 module.exports = server;
